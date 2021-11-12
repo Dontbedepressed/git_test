7dd53f9cc9e028b5e2f1b5fb683f9dd734afcfc4
20686 <2068608887@qq.com>
2021-11-13 07:29:36 +0800

    first commit

---
+++ git/src/main/java/com/atguigu/input.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @auther
+ * @create 2021-11-13-7:27
+ */
+public class input {
+    public static void main(String[] args) {
+        System.out.println("111111111111");
+    }
+}
